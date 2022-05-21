0bb3a87f054af8d89963055624002e3c05b29418
fchoi1 <masterfabster2010@hotmail.com>
2022-05-21 18:19:16 -0400

    initial

---
+++ server/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+// const db = require('./config/connection');
+// const routes = require('./routes');
+
+// const { authMiddleware } = require('./utils/auth');
+// const { ApolloServer } = require('apollo-server-express');
+
+// const { typeDefs, resolvers } = require('./schemas');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// const startServer = async () => {
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     context: authMiddleware
+//   });
+
+//   await server.start();
+//   server.applyMiddleware({ app });
+
+//   console.log(`Use GraphQ at http://localhost:${PORT}${server.graphqlPath}`);
+// };
+
+// startServer();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+// serve up react front-end in production
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+});
+
+// app.use(routes);
+
+// db.once('open', () => {
+app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
