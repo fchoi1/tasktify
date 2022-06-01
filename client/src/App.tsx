8e5e18e425e609495310c863e6f3c43de48b0f6d
fchoi1 <masterfabster2010@hotmail.com>
2022-05-31 20:03:52 -0400

    testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">Tasktify</div>;
+  return <div className="App">Tasktify Test</div>;
 }
 
 export default App;
